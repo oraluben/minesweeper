8c59f956d95dabc28ab85ff90b22fa0ebd59ba43
Yichen Yan <yichen.yan@intel.com>
2015-12-23 15:04:51 +0800

    init commit, use basic signalr functions

---
+++ SignalRTest/MineHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRTest
+{
+    public class MineHub : Hub
+    {
+        public void SendToServer(string s_json)
+        {
+            System.Diagnostics.Debug.WriteLine("call SendToServer");
+            Clients.All.sendToClient(s_json);
+        }
+
+        override public Task OnConnected()
+        { return null; }
+        override public Task OnDisconnected(bool stopCalled)
+        { return null; }
+        override public Task OnReconnected()
+        { return null; }
+    }
+}
---
+++ SignalRTest/Scripts/index.js
@@ -0,0 +1,42 @@
+﻿var mine = Vue.component('mine', {
+    template: '<canvas></canvas>',
+    data: function () {
+        return {
+            d0: 1,
+        };
+    },
+    methods: {
+        send_to_server: function (s_json) {
+            console.warn('init send_to_server called');
+        },
+        receive_from_server: function (s_json) {
+            console.log(s_json);
+            // todo
+        },
+        send_to_server_factory: function (hub) {
+            return function (s_json) {
+                console.assert(hub != undefined, 'hub is undefined');
+                hub.server.sendToServer(s_json);
+            };
+        },
+    },
+    compiled: function () {
+        var self = this;
+
+        var mine_hub = $.connection.mineHub;
+        mine_hub.client.sendToClient = self.receive_from_server;
+
+        $.connection.hub.start().done(function () {
+            self.send_to_server = self.send_to_server_factory(mine_hub);
+        });
+
+        console.log(self, mine_hub);
+        // todo: init visiable mine data
+    },
+});
+
+(function () {
+    new Vue({
+        el: '#mine',
+    });
+})();
