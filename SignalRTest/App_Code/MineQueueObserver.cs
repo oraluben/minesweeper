7d6feab0d9aef3771ef995c35fec0317ed94801b
Yichen <or.ange.l.evol@gmail.com>
2016-01-03 19:26:45 +0800

    create a branch use var to remember data, used for testing

--- SignalRTest/App_Code/MineQueueObserver.cs
+++ SignalRTest/App_Code/MineQueueObserver.cs
@@ -4,6 +4,7 @@ using MineGroup;
 using Newtonsoft.Json;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -60,7 +61,7 @@ namespace SignalRTest.App_Code
                         int group_x = d.group_x;
                         int group_y = d.group_y;
 
-                        using (MineGroupUtils m = new MineGroupUtils())
+                        using (TestData m = new TestData())
                         {
                             d.data = m.getInit(group_x, group_y);
                         }
@@ -74,7 +75,7 @@ namespace SignalRTest.App_Code
                     {
                         ClickParamMessage d = JsonConvert.DeserializeObject<ClickParamMessage>(body_o.param);
 
-                        using (MineGroupUtils context = new MineGroupUtils())
+                        using (TestData context = new TestData())
                         {
                             switch (d.data)  // "left" or "right"
                             {
@@ -98,6 +99,82 @@ namespace SignalRTest.App_Code
                         break;
                     }
             }
+        }
+    }
+
+    public class TestData : IDisposable
+    {
+        static Dictionary<int, Dictionary<int, string>> data = new Dictionary<int, Dictionary<int, string>>();
+        static Random seed = new Random();
+
+        public void Dispose()
+        {
+            return;
+        }
+
+        public string randomGroup()
+        {
+            ArrayList res = ArrayList.Repeat(false, 400);
+            for (int i = 0; i < 400; i++)
+            {
+                if (seed.NextDouble() < 0.2) res[i] = true;
+            }
+            return JsonConvert.SerializeObject(res);
+        }
+
+        public bool isMine(int mine_x, int mine_y)
+        {
+            int offset_x = mine_x % 20, offset_y = mine_y % 20;
+            if (offset_x < 0) offset_x += 20;
+            if (offset_y < 0) offset_y += 20;
+            int group_x = mine_x - offset_x, group_y = mine_y - offset_y;
+
+            if (!data.ContainsKey(group_x)) { data.Add(group_x, new Dictionary<int, string>()); }
+            if (!data[group_x].ContainsKey(group_y)) { data[group_x].Add(group_y, randomGroup()); }
+
+            ArrayList res = JsonConvert.DeserializeObject<ArrayList>(data[group_x][group_y]);
+
+            return (bool)res[offset_x + offset_y * 20];
+        }
+
+        public int mineCount(int mine_x, int mine_y)
+        {
+            if (isMine(mine_x, mine_y)) return -1;
+            int res = 0;
+            for (int _x = -1; _x < 2; _x++)
+            {
+                for (int _y = -1; _y < 2; _y++)
+                {
+                    if (_x == 0 && _y == 0) continue;
+                    if (isMine(mine_x + _x, mine_y + _y)) res++;
+                }
+            }
+            return res;
+        }
+
+        internal string clickLeft(int mine_x, int mine_y)
+        {
+            MineInfo m = new MineInfo(mine_x, mine_y);
+            m.val = mineCount(mine_x, mine_y);
+            if (m.val < 0) m.val = -2;
+            else m.val += 1;
+
+            return JsonConvert.SerializeObject(ArrayList.Repeat(m, 1));
+        }
+
+        internal string clickRight(int mine_x, int mine_y)
+        {
+            MineInfo m = new MineInfo(mine_x, mine_y);
+            m.val = mineCount(mine_x, mine_y);
+            if (m.val < 0) m.val = -1;
+            else m.val = 0;
+
+            return JsonConvert.SerializeObject(ArrayList.Repeat(m, 1));
+        }
+
+        internal string getInit(int group_x, int group_y)
+        {
+            return JsonConvert.SerializeObject(ArrayList.Repeat(0, 400));
         }
     }
 
